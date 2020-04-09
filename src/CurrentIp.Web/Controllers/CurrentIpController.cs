8f365d852aafabd4a4a114c83c225f72af65bc70
Anton Sizikov <sizikov.anton@gmail.com>
2020-04-09 13:05:33 +0200

    generate rdp file

--- src/CurrentIp.Web/Controllers/CurrentIpController.cs
+++ src/CurrentIp.Web/Controllers/CurrentIpController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Net;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using CurrentIp.DataModel;
@@ -28,15 +30,15 @@ namespace CurrentIp.Web.Controllers {
       return StatusCode(StatusCodes.Status201Created);
     }
 
-    [HttpGet("{machineName}/history")]
-    public async Task<ActionResult<IEnumerable<IpRecord>>> GetHistory(string machineName, CancellationToken token) {
-      var history = await _recordsRepository.GetHistoryAsync(machineName, token).ConfigureAwait(false);
+    [HttpGet("{machineTag}/history")]
+    public async Task<ActionResult<IEnumerable<IpRecord>>> GetHistory(string machineTag, CancellationToken token) {
+      var history = await _recordsRepository.GetHistoryAsync(machineTag, token).ConfigureAwait(false);
       return new OkObjectResult(history);
     }
 
-    [HttpGet("{machineName}/latest")]
-    public async Task<ActionResult<IpRecord>> GetLatest(string machineName, CancellationToken token) {
-      var latestRecord = await _recordsRepository.GetLatestAsync(machineName,token).ConfigureAwait(false);
+    [HttpGet("{machineTag}/latest")]
+    public async Task<ActionResult<IpRecord>> GetLatest(string machineTag, CancellationToken token) {
+      var latestRecord = await _recordsRepository.GetLatestAsync(machineTag,token).ConfigureAwait(false);
       if (latestRecord is null) {
         return new IpRecord {
           CurrentIP = new IPAddress(new byte[] {192, 168, 1, 1}).ToString(),
@@ -46,6 +48,18 @@ namespace CurrentIp.Web.Controllers {
       }
 
       return latestRecord;
+    }
+
+    [HttpGet("{machineTag}/rdp")]
+    public async Task<IActionResult> GetRdpFile(string machineTag, CancellationToken token) {
+      var latestRecord = await _recordsRepository.GetLatestAsync(machineTag, token).ConfigureAwait(false);
+      var sb = new StringBuilder();
+      sb.AppendLine($"full address:s:{latestRecord.CurrentIP}:3389");
+      sb.AppendLine("prompt for credentials:i:1");
+      sb.AppendLine("administrative session:i:1");
+
+      var stream = new MemoryStream(Encoding.ASCII.GetBytes(sb.ToString()));
+      return File(stream, "application/octet-stream",$"{latestRecord.MachineName}.rdp");
     }
   }
 }
